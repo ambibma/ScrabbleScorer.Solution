255ed124424c2464bba7d4c911e60ee32bf94205
ambi hidalgo <ambio.pk@gmail.com>
2023-02-06 20:28:31 -0800

    Test 1 ToPoints Fail expectedly
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Dani Steely <danisteelyart@gmail.com>
    Co-authored-by: Ariel Doyle  <ariel.rose.garrett@gmail.com>

--- ScrabbleScorer.Tests/ModelTests/WordScore.Tests.cs
+++ ScrabbleScorer.Tests/ModelTests/WordScore.Tests.cs
@@ -3,5 +3,20 @@ using System.Collections.Generic;
 using Scoring.Models;
 using System;
 
-
-
+namespace Scoring.Tests
+{
+  [TestClass]
+  public class ScoreTests
+  {
+    [TestMethod]
+    public void ToPoints_ReturnsPoints_Int()
+    {
+      //arrange
+      string word = "A";
+      //act
+      int result = WordScore.ToPoints(word);
+      //assert
+      Assert.AreEqual(1, result);
+    }
+  }
+}
